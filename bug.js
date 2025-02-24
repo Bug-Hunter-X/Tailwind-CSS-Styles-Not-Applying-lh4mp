e52086bf8365c232b4a291c953acc73373d47d5f
BugHunter-X <askzenai@gmail.com>
2025-02-24 18:30:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+//Example of Tailwind CSS not applying styles due to incorrect configuration.
+/*This is wrong*/
+<div className="bg-red-500 p-4">
+  <p>This text should be red.</p>
+</div>
+```
